156dc5c21ed1b77a1c7a0cce46b2c08c91f72b25
Tim Massingham <tim.massingham@nanoporetech.com>
2016-06-27 14:20:07 +0100

    Support for changing tag of features to read

--- sloika/features.py
+++ sloika/features.py
@@ -2,19 +2,21 @@ import numpy as np
 from sloika import sloika_dtype, layers
 from untangled.maths import studentise
 
-def from_events(ev):
+def from_events(ev, tag='scaled_'):
     """  Create a matrix of features from
+
     :param ev: A :class:ndrecarray with fields 'mean', 'stdv' and 'length'
+    :param tag: Prefix of which fields to read
 
     :Returns: A :class:ndarray with studentised features
     """
     nev = len(ev)
     features = np.zeros((nev, 4), dtype=sloika_dtype)
-    features[:,0] = ev['mean']
-    features[:,1] = ev['stdv']
+    features[:,0] = ev[tag + 'mean']
+    features[:,1] = ev[tag + 'stdv']
     features[:,2] = ev['length']
     #  Zero pad delta mean
-    features[:,3] = np.fabs(np.ediff1d(ev['mean'], to_end=0))
+    features[:,3] = np.fabs(np.ediff1d(ev[tag + 'mean'], to_end=0))
 
     features = np.ascontiguousarray(studentise(features, axis=0), dtype=sloika_dtype)
     return features
