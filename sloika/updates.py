cc85fb35aacc8b6674c952ddbe5ff1494e33675a
Tim Massingham <tim.massingham@nanoporetech.com>
2016-08-31 09:14:07 +0100

    Final changes to ADAMski
    Documentation
    Minor tweak to calculation of constant

--- sloika/updates.py
+++ sloika/updates.py
@@ -31,22 +31,32 @@ def sgd(network, loss, rate, momentum, clip=5.0):
 
     return updates
 
-def adam(network, loss, rate, decay, epsilon=1e-8, clip=5.0):
-    """  ADAM optimiser
+def adam(network, loss, rate, decay, epsilon=1e-8, clip=5.0, mrate=0.0005):
+    """  ADAMski optimiser
+
+    See: https://wiki/display/~tmassingham/2016/08/30/ADAMski
 
     :param network: network to optimise
     :param loss: loss function to optimise over
     :param rate: rate (step size) for optimiser
     :param decay: decay for estimate of gradient and curvature
     :param epsilon: same parameter to prevent reciprocal of variance exploding
+    :param mrate: Rate at which momentum is increased.  None = ADAM optimiser
 
     :returns: a dictionary containing update functions for Tensors
     """
     assert decay > (0.0, 0.0), "Decay must be non-negative"
     assert decay < (1.0, 1.0), "Decay must be less-than or equal to one"
-    _M_RATE = np.float32(-1.0 / 2000.0).astype(sloika_dtype)
-    _M_P = np.exp(_M_RATE)
-    _M_K = np.float32((1.0 - decay[0]) * decay[0] * _M_P / (1.0 - _M_P * decay[0])).astype(sloika_dtype)
+    assert mrate is None or mrate > 0.0, "Rate of momentum increase must be positive"
+    if mrate is not None:
+        _M_RATE = -np.float_(mrate).astype(sloika_dtype)
+        _M_P = np.exp(_M_RATE)
+        _M_K = (1.0 - decay[0]) * decay[0] * _M_P / (1.0 - _M_P * decay[0])
+        _M_K = np.float_(_M_K).astype(sloika_dtype)
+    else:
+        _M_RATE = -np.float_(1e30).astype(sloika_dtype)
+        _M_P = np.float_(0.0).astype(sloika_dtype)
+        _M_K = np.float_(0.0).astype(sloika_dtype)
 
     params = network.params()
     updates = OrderedDict()
@@ -58,9 +68,9 @@ def adam(network, loss, rate, decay, epsilon=1e-8, clip=5.0):
     lr_t = th.shared(np.float32(0.0).astype(sloika_dtype))
     momentum_decay = th.shared(np.float32(0.0).astype(sloika_dtype))
     updates[t] = t + 1.0
-    momentum_factor = _M_K * T.expm1(updates[t] * (ldecay[0] - _M_RATE)) - T.expm1(updates[t] * ldecay[0])
+    momentum_factor = _M_K * T.expm1(t * (ldecay[0] + _M_RATE)) - T.expm1(updates[t] * ldecay[0])
     updates[lr_t] = rate * T.sqrt(-T.expm1(updates[t] * ldecay[1])) / momentum_factor
-    updates[momentum_decay] = decay[0] * T.expm1(updates[t] * _M_RATE)
+    updates[momentum_decay] = -decay[0] * T.expm1(updates[t] * _M_RATE)
     for param, grad in zip(params, gradients):
         val = param.get_value(borrow=True)
         momentum = th.shared(np.zeros(val.shape, dtype=val.dtype))
@@ -68,7 +78,7 @@ def adam(network, loss, rate, decay, epsilon=1e-8, clip=5.0):
 
         grad_clip = T.clip(grad, -clip, clip)
 
-        updates[momentum] = updates[momentum_decay] * momentum + (1.0 - updates[momentum_decay]) * grad_clip
+        updates[momentum] = updates[momentum_decay] * momentum + (1.0 - decay[0]) * grad_clip
         updates[variance] = decay[1] * variance + (1.0 - decay[1]) * T.sqr(grad_clip)
         updates[param] = param - updates[lr_t] * updates[momentum] / (T.sqrt(updates[variance]) + epsilon)
 
