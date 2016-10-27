66d6228e31a99e2e1d73a02a2aba4955f4f32b9d
Tim Massingham <tim.massingham@nanoporetech.com>
2016-10-27 09:54:41 +0100

    Improve tests for fast5 reading

--- sloika/test/test_fast5.py
+++ sloika/test/test_fast5.py
@@ -1,4 +1,6 @@
 from fast5_research import fast5
+import glob
+import os
 import unittest
 
 class fast5Test(unittest.TestCase):
@@ -8,6 +10,10 @@ class fast5Test(unittest.TestCase):
         self.readdir = os.path.join(os.path.dirname(__file__), 'data', 'reads')
         self.filename = os.path.join(self.readdir, 'read03.fast5')
         self.section ='template'
+        self.strand_list = os.path.join(os.path.dirname(__file__), 'data',
+                                        'strands.txt')
+        self.strands = set(map(lambda r: os.path.join(self.readdir, r),
+                               ['read03.fast5', 'read16.fast5']))
 
     def test_001_get_mapping_data(self):
         #  Interface used by batch.py
@@ -20,10 +26,17 @@ class fast5Test(unittest.TestCase):
             ev = f5.get_section_events(self.section)
             sn = f5.filename_short
 
-    """
-    def test_003_iterate_fast5(self):
-        #  Iteration used
-        fast5_files = set(fast5.iterate_fast5(args.input_folder, paths=True,
-                                              limit=args.limit,
-                                              strand_list=args.strand_list))
-    """
+    def test_003_iterate_returns_all(self):
+        fast5_files = set(fast5.iterate_fast5(self.readdir, paths=True))
+        dir_list = set(glob.glob(os.path.join(self.readdir,'*.fast5')))
+        self.assertTrue(fast5_files == dir_list)
+
+    def test_004_iterate_respects_limits(self):
+        _LIMIT = 2
+        fast5_files = set(fast5.iterate_fast5(self.readdir, paths=True, limit=_LIMIT))
+        self.assertTrue(len(fast5_files) == _LIMIT)
+
+    def test_005_iterate_works_with_strandlist(self):
+        fast5_files = set(fast5.iterate_fast5(self.readdir, paths=True,
+                                              strand_list=self.strand_list))
+        self.assertTrue(self.strands == fast5_files)
