1ca92b84bd336ad305ea0a38661bc00cd1c146b7
Joe Harvey <joe.harvey@nanoporetech.com>
2016-11-01 14:20:11 +0000

    Fixed calls to SCRN.__init__ in models/baseline_scrn.py

--- models/baseline_scrn.py
+++ models/baseline_scrn.py
@@ -17,24 +17,20 @@ def network(klen, sd, nfeature=4, winlen=3, fast_size=32, slow_size=32):
 
     _prn = smt.partial(smt._rn, sd=sd)
     nstate = (smt._NBASE ** klen) + 1
-    rnn_act = smt.tanh
+    rnn_act = smt.sigmoid
     ff_act = smt.tanh
     insize = nfeature * winlen
 
     inlayer = smt.Window(winlen)
 
-    fwd1 = smt.SCRN(insize, fast_size, slow_size, init=_prn, has_bias=True,
-                       has_peep=True, fun=rnn_act)
-    bwd1 = smt.SCRN(insize, fast_size, slow_size, init=_prn, has_bias=True,
-                       has_peep=True, fun=rnn_act)
+    fwd1 = smt.SCRN(insize, fast_size, slow_size, init=_prn, fun=rnn_act)
+    bwd1 = smt.SCRN(insize, fast_size, slow_size, init=_prn, fun=rnn_act)
     layer1 = smt.birnn(fwd1, bwd1)
 
     layer2 = smt.FeedForward(2 * size, size, has_bias=True, fun=ff_act)
 
-    fwd3 = smt.SCRN(size, fast_size, slow_size, init=_prn, has_bias=True,
-                       has_peep=True, fun=rnn_act)
-    bwd3 = smt.SCRN(size, fast_size, slow_size, init=_prn, has_bias=True,
-                       has_peep=True, fun=rnn_act)
+    fwd3 = smt.SCRN(size, fast_size, slow_size, init=_prn, fun=rnn_act)
+    bwd3 = smt.SCRN(size, fast_size, slow_size, init=_prn, fun=rnn_act)
     layer3 = smt.birnn(fwd3, bwd3)
 
     layer4 = smt.FeedForward(2 * size, size, init=_prn, has_bias=True, fun=ff_act)
