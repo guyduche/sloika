3dd398117cf63a5bfffba0e6e554bac64b412812
Joe Harvey <joe.harvey@nanoporetech.com>
2016-11-07 17:01:01 +0000

    Added models for Mut2, Mut3, LstmO, and LstmCIFG

---
+++ models/baseline_mut3.py
@@ -0,0 +1,41 @@
+import sloika.module_tools as smt
+
+def network(klen, sd, nfeature=4, winlen=3, size=64):
+    """ Create standard Nanonet with LSTM recurrent units
+
+    :param klen: Length of kmer
+    :param sd: Standard Deviation of initialisation noise
+    :param nfeature: Number of features
+    :param winlen: Length of window over data
+    :param size: size of hidden recurrent layers
+
+    :returns: a `class`:layer.Layer:
+    """
+
+    _prn = smt.partial(smt._rn, sd=sd)
+    nstate = (smt._NBASE ** klen) + 1
+    rnn_act = smt.tanh
+    ff_act = smt.tanh
+    insize = nfeature * winlen
+
+    inlayer = smt.Window(winlen)
+
+    fwd1 = smt.Mut3(insize, size, init=_prn, has_bias=True,
+                       fun=rnn_act)
+    bwd1 = smt.Mut3(insize, size, init=_prn, has_bias=True,
+                       fun=rnn_act)
+    layer1 = smt.birnn(fwd1, bwd1)
+
+    layer2 = smt.FeedForward(2 * size, size, has_bias=True, fun=ff_act)
+
+    fwd3 = smt.Mut3(size, size, init=_prn, has_bias=True,
+                       fun=rnn_act)
+    bwd3 = smt.Mut3(size, size, init=_prn, has_bias=True,
+                       fun=rnn_act)
+    layer3 = smt.birnn(fwd3, bwd3)
+
+    layer4 = smt.FeedForward(2 * size, size, init=_prn, has_bias=True, fun=ff_act)
+
+    outlayer = smt.Softmax(size, nstate, init=_prn, has_bias=True)
+
+    return smt.Serial([inlayer, layer1, layer2, layer3, layer4, outlayer])
