f8477f01d6d2452e74462ab5f3a40d293450f338
Semen Trygubenko <semen.trygubenko@nanoporetech.com>
2017-01-04 11:10:28 +0000

    When installing into virtualenv sudo is not required

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -7,7 +7,7 @@ source environment
 
 mkdir -p ${SLOIKA_SITE_PACKAGES}
 
-sudo pip install -r requirements.txt --trusted-host pypi.oxfordnanolabs.local \
+pip install -r requirements.txt --trusted-host pypi.oxfordnanolabs.local \
 	-i https://pypi.oxfordnanolabs.local/simple/ 
 
 python setup.py develop --prefix ${SLOIKA_DEV_DIR}
