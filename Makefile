f148b8404d9a19a166aca9d1307f7d670521bcd1
Semen Trygubenko <semen.trygubenko@nanoporetech.com>
2017-01-06 15:12:45 +0000

    Bug fix: make use of virtualenv when developing + refactoring
    * bug fixed: install sloika into virtualenv when in development mode
    * make `environment` self-sufficient by moving definition of BUILD_DIR into it
    * new targets `newEnv` and `clean`

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+SHELL=/bin/bash
+
 pwd:=$(shell pwd)/
 bin:=${pwd}bin/
 
@@ -7,3 +9,11 @@ bin:=${pwd}bin/
 .PHONY: train
 train:
 	${bin}create_hdf5.py --chunk 500 --kmer 5 --section template --strand_list train_strand_list.txt  fast5_directory dataset_train.hdf5
+
+.PHONY: newEnv
+newEnv: clean
+	./setup-dev-env.sh
+
+.PHONY: clean
+clean:
+	(source environment && rm -rf ${BUILD_DIR})
--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -ex
 
-export ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 source environment
 
+# for fromscratch builds plow through ${SLOIKA_VIRTUALENV_DIR} before running this script
+mkdir -p ${SLOIKA_VIRTUALENV_DIR}
+virtualenv ${SLOIKA_VIRTUALENV_DIR}
 
-mkdir -p ${SLOIKA_SITE_PACKAGES}
+source ${SLOIKA_VIRTUALENV_DIR}/bin/activate
+
+# need a version of pip that supports --trusted-host option
+pip install pip --upgrade
 
 pip install -r requirements.txt --trusted-host pypi.oxfordnanolabs.local \
 	-i https://pypi.oxfordnanolabs.local/simple/ 
 
-python setup.py develop --prefix ${SLOIKA_DEV_DIR}
+python setup.py develop
