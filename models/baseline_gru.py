cc91a33eb4454ca863313c8d58fd8c3e608f9a8e
Tim Massingham <tim.massingham@nanoporetech.com>
2017-01-09 09:55:36 +0000

    Update model files to use new routines that encapsulates NBASE

--- models/baseline_gru.py
+++ models/baseline_gru.py
@@ -13,7 +13,7 @@ def network(klen, sd, nfeature=4, winlen=3, size=64):
     :returns: a `class`:layer.Layer:
     """
     _prn = smt.partial(smt._rn, sd=sd)
-    nstate = (smt._NBASE ** klen) + 1
+    nstate = smt.nstate(nlen)
     gru_act = smt.tanh
     ff_act = smt.tanh
 
--- models/poormans_nanonet.py
+++ models/poormans_nanonet.py
@@ -13,7 +13,7 @@ def network(klen, sd, nfeature=4, winlen=3, size=64):
     :returns: a `class`:layer.Layer:
     """
     _prn = smt.partial(smt._rn, sd=sd)
-    nstate = (smt._NBASE ** klen) + 1
+    nstate = smt.nstate(klen)
     gru_act = smt.tanh_pm
     ff_act = smt.tanh_pm
     gate_act = smt.sigmoid_pm
