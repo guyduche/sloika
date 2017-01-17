6cc13e66a779ba425fe38c5fb586412cd3bfc16b
Semen Trygubenko <semen.trygubenko@nanoporetech.com>
2017-01-17 15:54:20 +0000

    Bug fixed: incorrect patch number
    Previously didn't correspond to the number of commits in a branch.

--- scripts/version-env.sh
+++ scripts/version-env.sh
@@ -4,7 +4,7 @@ export SLOIKA_VERSION_MAJOR=1
 export SLOIKA_VERSION_MINOR=1
 
 if hash git 2>/dev/null; then
-    export SLOIKA_VERSION_PATCH=$(git rev-list --all --count)
+    export SLOIKA_VERSION_PATCH=$(git rev-list --count HEAD)
 else
     export SLOIKA_VERSION_PATCH=0
 fi
