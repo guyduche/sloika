57ce2ae27c40268f713111676dd2f8f736352adf
Semen Trygubenko <semen.trygubenko@nanoporetech.com>
2017-02-07 10:56:14 +0000

    Documenting behavior of chunkify with remap in a test

--- test/acceptance/test_chunkify.py
+++ test/acceptance/test_chunkify.py
@@ -28,6 +28,11 @@ class AcceptanceTest(unittest.TestCase):
 
         self.data_dir = os.path.join(os.environ["DATA_DIR"], self.test_name)
 
+    def assertClose(self, a, b):
+        if not is_close(a, b, 1e-5):
+            msg = '{} is not close {}'.format(a, b)
+            self.assertTrue(is_close(a, b, 1e-5), msg)
+
     def test_commands(self):
         cmd = [self.script]
         run_cmd(self, cmd).return_code(0).stdout(lambda o: drop_info(o).startswith(u"Available commands:"))
@@ -41,14 +46,14 @@ class AcceptanceTest(unittest.TestCase):
         cmd = [self.script, "hehe"]
         run_cmd(self, cmd).return_code(1).stdout(lambda o: drop_info(o).startswith(u"Unsupported command 'hehe'"))
 
-    def test_identity(self):
+    def test_chunkify_identity(self):
         strand_input_list = os.path.join(self.data_dir, "identity", "na12878_train.txt")
         self.assertTrue(os.path.exists(strand_input_list))
 
         reads_dir = os.path.join(self.data_dir, "identity", "reads")
         self.assertTrue(os.path.exists(reads_dir))
 
-        output_file = os.path.join(self.work_dir, "dataset_train.hdf5")
+        output_file = os.path.join(self.work_dir, "chunkify_identity.hdf5")
         if os.path.exists(output_file):
             os.remove(output_file)
 
@@ -66,8 +71,38 @@ class AcceptanceTest(unittest.TestCase):
 
             self.assertEqual(fh['chunks'].shape, (182, 500, 4))
             chunks = fh['chunks'][:]
-            self.assertTrue(is_close(chunks.min(), -2.8844583, 1e-5))
-            self.assertTrue(is_close(chunks.max(), 14.225174, 1e-5))
+            self.assertClose(chunks.min(), -2.8844583)
+            self.assertClose(chunks.max(), 14.225174)
+            self.assertClose(np.median(chunks), -0.254353493452)
+
+    def test_chunkify_identity2(self):
+        strand_input_list = os.path.join(self.data_dir, "remap", "strand_output_list.txt")
+        self.assertTrue(os.path.exists(strand_input_list))
+
+        reads_dir = os.path.join(self.data_dir, "remap", "remappedReads")
+        self.assertTrue(os.path.exists(reads_dir))
+
+        output_file = os.path.join(self.work_dir, "chunkify_identity2.hdf5")
+        if os.path.exists(output_file):
+            os.remove(output_file)
+
+        cmd = [self.script, "identity", "--use_scaled", "--chunk", "500", "--kmer", "5",
+               "--section", "template", "--strand_list", strand_input_list,
+               reads_dir, output_file]
+        run_cmd(self, cmd).return_code(0)
+
+        with h5py.File(output_file, 'r') as fh:
+            top_level_items = []
+            for item in fh:
+                top_level_items.append(item)
+            top_level_items.sort()
+            self.assertEqual(top_level_items, [u'bad', u'chunks', u'labels', u'weights'])
+
+            self.assertEqual(fh['chunks'].shape, (32, 500, 4))
+            chunks = fh['chunks'][:]
+            self.assertClose(chunks.min(), -2.70104813576)
+            self.assertClose(chunks.max(), 12.7622556686)
+            self.assertClose(np.median(chunks), -0.234752029181)
 
     def test_remap(self):
         strand_output_list = os.path.join(self.work_dir, "strand_output_list.txt")
