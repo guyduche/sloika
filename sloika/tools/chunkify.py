3f44187fd7cee096d51275c9cf8988cc889691fe
Semen Trygubenko <semen.trygubenko@nanoporetech.com>
2017-02-07 11:53:59 +0000

    Factor out functionality for creation of hdf5 file

--- sloika/tools/chunkify.py
+++ sloika/tools/chunkify.py
@@ -24,6 +24,38 @@ def progress_report(i):
     return i
 
 
+def create_hdf5(args, all_chunks, all_labels, all_bad):
+    print('\n* Writing out to HDF5')
+
+    output_dir = os.path.dirname(args.output)
+    if output_dir and not os.path.exists(output_dir):
+        os.makedirs(os.path.normpath(output_dir))
+
+    #  Mark chunks with too many blanks with a zero weight
+    nblank = np.sum(all_labels == 0, axis=1)
+    max_blanks = int(all_labels.shape[1] * args.blanks)
+    all_weights = nblank < max_blanks
+
+    with h5py.File(args.output, 'w') as h5:
+        bad_ds = h5.create_dataset('bad', all_bad.shape, dtype='i1',
+                                   compression="gzip")
+        chunk_ds = h5.create_dataset('chunks', all_chunks.shape, dtype='f4',
+                                     compression="gzip")
+        label_ds = h5.create_dataset('labels', all_labels.shape, dtype='i4',
+                                     compression="gzip")
+        weight_ds = h5.create_dataset('weights', all_weights.shape, dtype='f4',
+                                      compression="gzip")
+        bad_ds[:] = all_bad
+        chunk_ds[:] = all_chunks
+        label_ds[:] = all_labels
+        weight_ds[:] = all_weights
+        h5['/'].attrs['chunk'] = args.chunk
+        h5['/'].attrs['kmer'] = args.kmer
+        h5['/'].attrs['section'] = args.section
+        h5['/'].attrs['trim'] = args.trim
+        h5['/'].attrs['scaled'] = args.use_scaled
+
+
 def chunkify_main(argv):
     program_name = ' '.join(sys.argv[:2])
 
@@ -89,32 +121,4 @@ def chunkify_main(argv):
     all_labels = np.vstack(label_list)
     all_bad = np.vstack(bad_list)
 
-    #  Mark chunks with too many blanks with a zero weight
-    nblank = np.sum(all_labels == 0, axis=1)
-    max_blanks = int(all_labels.shape[1] * args.blanks)
-    all_weights = nblank < max_blanks
-
-    print('\n* Writing out to HDF5')
-
-    output_dir = os.path.dirname(args.output)
-    if output_dir and not os.path.exists(output_dir):
-        os.makedirs(os.path.normpath(output_dir))
-
-    with h5py.File(args.output, 'w') as h5:
-        bad_ds = h5.create_dataset('bad', all_bad.shape, dtype='i1',
-                                   compression="gzip")
-        chunk_ds = h5.create_dataset('chunks', all_chunks.shape, dtype='f4',
-                                     compression="gzip")
-        label_ds = h5.create_dataset('labels', all_labels.shape, dtype='i4',
-                                     compression="gzip")
-        weight_ds = h5.create_dataset('weights', all_weights.shape, dtype='f4',
-                                      compression="gzip")
-        bad_ds[:] = all_bad
-        chunk_ds[:] = all_chunks
-        label_ds[:] = all_labels
-        weight_ds[:] = all_weights
-        h5['/'].attrs['chunk'] = args.chunk
-        h5['/'].attrs['kmer'] = args.kmer
-        h5['/'].attrs['section'] = args.section
-        h5['/'].attrs['trim'] = args.trim
-        h5['/'].attrs['scaled'] = args.use_scaled
+    create_hdf5(args, all_chunks, all_labels, all_bad)
