69d605ce4ef95473761c5948b65fd13976962fce
Joe Harvey <joe.harvey@nanoporetech.com>
2017-02-10 15:53:05 +0000

    Align script

---
+++ misc/align.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+import csv
+from collections import OrderedDict
+import numpy as np
+import matplotlib.pyplot as plt
+import pysam
+from scipy.stats import gaussian_kde
+from scipy.optimize import minimize_scalar
+import subprocess
+import sys
+from untangled.cmdargs import proportion, FileExists
+
+
+parser = argparse.ArgumentParser(
+    description='Align reads to reference and output accuracy statistics',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--coverage', metavar='proportion', default=0.6, type=proportion,
+    help='Minimum coverage')
+parser.add_argument('--bwa_mem_args', metavar='args',
+    help="Command line arguments to pass to bwa mem")
+parser.add_argument('reference', action=FileExists,
+    help="Reference sequence to align against")
+parser.add_argument('files', metavar='seqs.fa', nargs='+',
+    help="One or more fasta files containing queries")
+
+
+STRAND = { 0 : '+',
+           16 : '-'}
+
+
+def call_bwa_mem(fin, fout, genome, clargs=None):
+    """Call bwa aligner using the subprocess module
+
+    :param fin: input sequence filename
+    :param fout: filename for the output sam file
+    :param genome: path to reference to align against
+    :param clargs: optional command line argumetns to pass to bwa as a string.
+        The default is: "-t 16 -A 1 -B 2 -O 2 -E 1"
+
+    :returns: stdout of bwa command
+
+    :raises: subprocess.CalledProcessError
+    """
+    clargs = clargs if clargs is not None else "-t 16 -A 1 -B 2 -O 2 -E 1"
+    command_line = "bwa mem {} {} {} > {}".format(clargs, genome, fin, fout)
+    try:
+        output = subprocess.check_output(command_line,
+                                         stderr=subprocess.STDOUT,
+                                         shell=True)
+    except subprocess.CalledProcessError as e:
+        sys.stderr.write("Error calling bwa, exit code {}\n".format(e.returncode))
+        sys.stderr.write(e.output + '\n')
+        raise
+    return output
+
+
+def samacc(sam, min_coverage=0.6):
+    """Read alignments from sam file and return accuracy metrics
+
+    :param sam: filename of input sam file
+    :min_coverage: alignments are filtered by coverage
+
+    :returns: list of row dictionaries with keys:
+        name1: reference name
+        name2: query name
+        strand: + or -
+        match: number of matches
+        mismatch: number of mismatches
+        insertion: number of insertions
+        deletion: number of deletions
+        coverage: query alignment length / query length
+        id: identity = sequence matches / alignment matches
+        accuracy: sequence matches / alignment length
+    """
+    res = []
+    with pysam.Samfile(sam, 'r') as sf:
+        ref_name = sf.references
+        for read in sf:
+            if read.flag != 0 and read.flag != 16:
+                continue
+
+            coverage = float(read.query_alignment_length) / read.query_length
+            if coverage < min_coverage:
+                continue
+
+            bins = np.zeros(9, dtype='i4')
+            for flag, count in read.cigar:
+                bins[flag] += count
+
+            tags = dict(read.tags)
+            alnlen = np.sum(bins[:3])
+            mismatch = tags['NM']
+            correct = alnlen - mismatch
+
+            row = OrderedDict([
+                ('name1', ref_name[read.reference_id]),
+                ('name2', read.qname),
+                ('strand', STRAND[read.flag]),
+                ('match', bins[0]),
+                ('mismatch', mismatch),
+                ('insertion', bins[1]),
+                ('deletion', bins[2]),
+                ('coverage', coverage),
+                ('id', float(correct) / float(bins[0])),
+                ('accuracy', float(correct) / alnlen),
+            ])
+            res.append(row)
+    return res
+
+
+def snort_prefix_suffix(filename):
+    """Extract filename prefix and extension, if any
+
+    E.g.
+        "prefix.suffix" --> ("prefix", "suffix")
+        "really.long.name" --> ("really.long", "name")
+        "no_extension_at_all" --> ("no_extension_at_all", "")
+    """
+    tokens = filename.split('.')
+    if len(tokens) > 1:
+        return ('.'.join(tokens[:-1]), tokens[-1])
+    else:
+        return (filename, '')
+
+
+def acc_plot(acc, mode, title="Test"):
+    """Plot accuracy histogram
+
+    :param acc_dat: list of row dictionaries of basecall accuracy data
+    :param title: plot title
+
+    :returns: (figure handle, axes handle)
+    """
+    f = plt.figure()
+    ax = f.add_subplot(111)
+    ax.hist(acc, bins=np.arange(0.65, 1.0, 0.01))
+    ax.set_xlim(0.65, 1)
+    _, ymax = ax.get_ylim()
+    ax.plot([mode, mode], [0, ymax], 'r--')
+    ax.set_xlabel("Accuracy")
+    ax.set_ylabel("Frequency")
+    ax.set_title(title)
+    return f, ax
+
+
+def summary(acc_dat, name):
+    """Crate summary report and plots for accuracy statistics
+
+    :param acc_dat: list of row dictionaries of read accuracy metrics
+    :param name: name for the data set
+
+    :returns: (report string, figure handle, axes handle)
+    """
+    if len(acc_dat) == 0:
+        res = """Summary report for {}:
+    No sequences mapped
+""".format(name)
+        return res, None, None
+
+    acc = np.array([r['accuracy'] for r in acc_dat])
+    mean = acc.mean()
+
+    da = gaussian_kde(acc)
+    mode = minimize_scalar(lambda x: -da(x), bounds=(0, 1)).x[0]
+
+    quantiles = np.percentile(acc, [5, 25, 50, 75, 95])
+    qstring = '{:.5f}    {:.5f}    {:.5f}    {:.5f}    {:.5f}'.format(*quantiles)
+
+    a90 = (acc > 0.9).mean()
+    n90 = (acc > 0.9).sum()
+    nmapped = len(set([r['name2'] for r in acc_dat]))
+
+    res = """Summary report for {}:
+    Number of mapped reads:  {}
+    Mean accuracy:  {:.5f}
+    Mode accuracy:  {:.5f}
+    Accuracy quantiles:      Q5        Q25        Q50        Q75        Q95
+                          {}
+    Proportion with accuracy >90%:  {:.5f}
+    Number with accuracy >90%:  {}
+""".format(name, nmapped, mean, mode, qstring, a90, n90)
+
+    title = "{} (n = {})".format(name, nmapped)
+    f, ax = acc_plot(acc, mode, title)
+    return res, f, ax
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    for fn in args.files:
+        prefix, suffix = snort_prefix_suffix(fn)
+        samfile = prefix + '.sam'
+        samaccfile = prefix + '.samacc'
+        summaryfile = prefix + '.summary'
+        graphfile = prefix + '.png'
+
+        # align sequences to reference
+        sys.stdout.write("Aligning {}...\n".format(fn))
+        bwa_output = call_bwa_mem(fn, samfile, args.reference, args.bwa_mem_args)
+        sys.stdout.write(bwa_output)
+
+        # compile accuracy metrics
+        acc_dat = samacc(samfile, min_coverage=args.coverage)
+        if len(acc_dat) > 0:
+            with open(samaccfile, 'w') as fs:
+                fields = acc_dat[0].keys()
+                writer = csv.DictWriter(fs, fieldnames=fields, delimiter=' ')
+                writer.writeheader()
+                for row in acc_dat:
+                    writer.writerow(row)
+
+        # write summary file and plot
+        report, f, ax = summary(acc_dat, name=fn)
+        if f is not None:
+            f.savefig(graphfile)
+        sys.stdout.write('\n' + report + '\n')
+        with open(summaryfile, 'w') as fs:
+            fs.writelines(report)
