6d4547fd5001d798e82a882d1d3ecd41d03ff9e1
Tim Massingham <tim.massingham@nanoporetech.com>
2017-02-23 12:20:44 +0000

    All layers should have insize and size

--- sloika/layers.py
+++ sloika/layers.py
@@ -1516,6 +1516,8 @@ class Reverse(Layer):
 
     def __init__(self, layer):
         self.layer = layer
+        self.insize = layer.insize
+        self.size = layer.size
 
     @property
     def insize(self):
