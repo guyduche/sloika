62acb1c825c8404779af83ed69b615b681001347
Semen Trygubenko <semen.trygubenko@nanoporetech.com>
2017-02-27 10:17:30 +0000

    Handle more gracefully no-chunks-to-write case

--- test/acceptance/test_chunkify.py
+++ test/acceptance/test_chunkify.py
@@ -146,3 +146,46 @@ class AcceptanceTest(unittest.TestCase):
 
         os.remove(output_file_name)
         os.remove(strand_output_list)
+
+    def test_chunkify_with_remap_no_results(self):
+        strand_input_list = os.path.join(self.data_dir, "remap", "strand_output_list.txt")
+        self.assertTrue(os.path.exists(strand_input_list))
+
+        reads_dir = os.path.join(self.data_dir, "identity", "reads")
+        self.assertTrue(os.path.exists(reads_dir))
+
+        model_file = os.path.join(self.data_dir, "remap", "model.pkl")
+        self.assertTrue(os.path.exists(model_file))
+
+        reference_file = os.path.join(self.data_dir, "remap", "reference.fa")
+        self.assertTrue(os.path.exists(reference_file))
+
+        with tempfile.NamedTemporaryFile(prefix="strand_output_list", suffix=".txt", delete=False) as fh:
+            strand_output_list = fh.name
+
+        with tempfile.NamedTemporaryFile(suffix=".hdf5", delete=False) as fh:
+            output_file_name = fh.name
+
+        cmd = [self.script, "remap", "--trim", "200", "200", "--chunk_len", "500", "--kmer_len", "5",
+               "--section", "template", "--input_strand_list", strand_input_list,
+               "--output_strand_list", strand_output_list, "--overwrite",
+               reads_dir, output_file_name, model_file, reference_file]
+
+        run_cmd(self, cmd).return_code(0)
+
+        self.assertTrue(os.path.exists(output_file_name))
+        with h5py.File(output_file_name, 'r') as fh:
+            top_level_items = []
+            for item in fh:
+                top_level_items.append(item)
+            top_level_items.sort()
+            self.assertEqual(top_level_items, [u'bad', u'chunks', u'labels', u'weights'])
+
+            self.assertEqual(fh['chunks'].shape, (0,))
+
+        os.remove(output_file_name)
+
+        self.assertTrue(os.path.exists(strand_output_list))
+        lines = open(strand_output_list, 'r').read()
+        self.assertEqual(lines, u'filename\tnev\tscore\tnstay\tseqlen\tstart\tend\n')
+        os.remove(strand_output_list)
