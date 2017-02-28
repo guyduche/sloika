6c56749662ea63598aae5ec1616c833b7c01379b
Semen Trygubenko <semen.trygubenko@nanoporetech.com>
2017-02-28 11:53:17 +0000

    Bug fixed: wrong exit code
    Was updated in one place but not the other during code review

--- sloika/tools/chunkify_with_identity.py
+++ sloika/tools/chunkify_with_identity.py
@@ -47,7 +47,7 @@ def chunkify_with_identity_main(argv, parser):
 
     if chunk_list == []:
         print("no chunks were produced", file=sys.stderr)
-        sys.exit(5)
+        sys.exit(1)
     else:
         print('\n* Writing out to HDF5')
         util.create_hdf5(args, chunk_list, label_list, bad_list)
