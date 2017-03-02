bc3e7e10060d60bdea1a435f90eaaa9dccd8d440
Joe Harvey <joe.harvey@nanoporetech.com>
2017-03-02 16:07:34 +0000

    Unscrap my boiler-plate

--- sloika/conv.py
+++ sloika/conv.py
@@ -1,3 +1,9 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
 from collections import OrderedDict
 import numpy as np
 import theano as th
