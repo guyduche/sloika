6ae0e18512d54491c8c6e65dc5c5777cb69d4feb
Semen Trygubenko <semen.trygubenko@nanoporetech.com>
2017-03-15 15:34:12 +0000

    Refactored to improve reuse and reduce complexity

--- bin/basecall_network.py
+++ bin/basecall_network.py
@@ -7,20 +7,17 @@ standard_library.install_aliases()
 from builtins import *
 
 import argparse
-import numpy as np
 import os
 import pickle
 import sys
 import time
 
-from untangled import bio, fast5
+from untangled import fast5
 from untangled.cmdargs import (AutoBool, FileAbsent, FileExists, Maybe,
                                NonNegative, proportion, Positive, Vector)
 from untangled.iterators import imap_mp
-from untangled.maths import mad
 
-from sloika import helpers, util
-from sloika.variables import nstate
+from sloika import basecall, helpers, util
 
 
 # create the top-level parser
@@ -83,112 +80,25 @@ parser_raw.add_argument('--trim', default=(200, 10), nargs=2, type=NonNegative(i
 parser_raw.set_defaults(datatype='samples')
 
 
-def init_worker(model):
-    import pickle
-    global calc_post
-    with open(model, 'rb') as fh:
-        calc_post = pickle.load(fh)
-
-
-def decode_post(post, args, eta=1e-10):
-    from sloika import decode
-    assert post.shape[2] == nstate(args.kmer_len, transducer=args.transducer, bad_state=args.bad)
-    post = decode.prepare_post(post, min_prob=args.min_prob, drop_bad=args.bad and not args.transducer)
-    if args.transducer:
-        score, call = decode.viterbi(post, args.kmer_len, skip_pen=args.skip)
-    else:
-        trans = olddecode.estimate_transitions(post, trans=args.trans)
-        score, call = olddecode.decode_profile(post, trans=np.log(eta + trans), log=False)
-    return score, call
-
-
-def basecall_events(args, fn):
-    from sloika import features
-    try:
-        with fast5.Reader(fn) as f5:
-            ev = f5.get_section_events(args.section, analysis=args.segmentation)
-            sn = f5.filename_short
-    except Exception as e:
-        sys.stderr.write("Error getting events for section {!r} in file {}\n{!r}\n".format(args.section, fn, e))
-        return None
-
-    ev = util.trim_array(ev, args.trim)
-    if ev.size == 0:
-        sys.stderr.write("Read too short in file {}\n".format(fn))
-        return None
-
-    inMat = features.from_events(ev, tag='')[:, None, :]
-    score, call = decode_post(calc_post(inMat), args)
-
-    return sn, score, call, inMat.shape[0]
-
-
-def basecall_raw(args, fn):
-    from sloika import batch, config
-    try:
-        with fast5.Reader(fn) as f5:
-            signal = f5.get_read(raw=True)
-            sn = f5.filename_short
-    except Exception as e:
-        sys.stderr.write("Error getting raw data for file {}\n{!r}\n".format(fn, e))
-        return None
-
-    signal = batch.trim_open_pore(signal, args.open_pore_fraction)
-    signal = util.trim_array(signal, args.trim)
-    if signal.size == 0:
-        sys.stderr.write("Read too short in file {}\n".format(fn))
-        return None
-
-    inMat = (signal - np.median(signal)) / mad(signal)
-    inMat = inMat[:, None, None].astype(config.sloika_dtype)
-    score, call = decode_post(calc_post(inMat), args)
-
-    return sn, score, call, inMat.shape[0]
-
-
-class SeqPrinter(object):
-
-    def __init__(self, kmerlen, datatype="events", transducer=False, fname=None):
-        self.kmers = bio.all_kmers(kmerlen)
-        self.transducer = transducer
-        self.datatype = datatype
-
-        if fname is None:
-            self.fh = sys.stdout
-            self.close_fh = False
-        else:
-            self.fh = open(fname, 'w')
-            self.close_fh = True
-
-    def __del__(self):
-        if self.close_fh:
-            self.fh.close()
-
-    def write(self, read_name, score, call, nev):
-        kmer_path = [self.kmers[i] for i in call]
-        seq = bio.kmers_to_sequence(kmer_path, always_move=self.transducer)
-        self.fh.write(">{} score {:.0f}, {} {} to {} bases\n".format(read_name, score,
-                                                                     nev, self.datatype, len(seq)))
-        self.fh.write(seq + '\n')
-        return len(seq)
-
-
 if __name__ == '__main__':
     args = parser.parse_args()
 
     assert args.command in ["events", "raw"]
+    assert args.datatype in ["events", "samples"]
+
+    basecall_worker = getattr(basecall, args.command + "_worker")
 
     compiled_file = helpers.compile_model(args.model, args.compile)
 
-    seq_printer = SeqPrinter(args.kmer_len, datatype=args.datatype,
-                             transducer=args.transducer)
+    seq_printer = basecall.SeqPrinter(args.kmer_len, datatype=args.datatype,
+                                      transducer=args.transducer)
 
     files = fast5.iterate_fast5(args.input_folder, paths=True, limit=args.limit,
                                 strand_list=args.input_strand_list)
     nbases = nevents = 0
     t0 = time.time()
-    for res in imap_mp(basecall, files, threads=args.jobs, fix_args=[args],
-                       unordered=True, init=init_worker, initargs=[compiled_file]):
+    for res in imap_mp(basecall_worker, files, threads=args.jobs, fix_args=[args],
+                       unordered=True, init=basecall.init_worker, initargs=[compiled_file]):
         if res is None:
             continue
         read, score, call, nev = res
