c862c40abc3e11de7f3703bfc1f1f6e321279e48
Semen Trygubenko <semen.trygubenko@nanoporetech.com>
2017-03-23 11:07:23 +0000

    Add a couple of raw acctest stubs

--- test/acceptance/test_train_network.py
+++ test/acceptance/test_train_network.py
@@ -58,9 +58,9 @@ class AcceptanceTest(unittest.TestCase):
         util.run_cmd(self, cmd).return_code(2).stderr(util.last_line_starts_with(msg))
 
     @parameterized.expand([
-        [[], "0"],
+        ["0"],
     ])
-    def test_baseline_lstm_training(self, options, subdir):
+    def test_baseline_lstm_training(self, subdir):
         test_work_dir = self.work_dir(os.path.join("test_baseline_lstm_training", subdir))
 
         strand_input_list = os.path.join(self.data_dir, "events", "na12878_train.txt")
@@ -73,7 +73,7 @@ class AcceptanceTest(unittest.TestCase):
 
         prepare_cmd = [self.chunkify_script, "identity", "--chunk_len", "500", "--kmer_len", "5",
                        "--section", "template", "--input_strand_list", strand_input_list,
-                       reads_dir, hdf5_file, "--overwrite"] + options
+                       reads_dir, hdf5_file, "--overwrite"]
 
         util.run_cmd(self, prepare_cmd).return_code(0)
 
@@ -91,8 +91,38 @@ class AcceptanceTest(unittest.TestCase):
         if os.path.exists(output_directory):
             shutil.rmtree(output_directory)
 
-        train_cmd = [self.script, "events", "--batch", "100", "--niteration", "1", "--save_every", "1",
+        train_cmd = [self.script, "events", "--batch_size", "100", "--niteration", "1", "--save_every", "1",
                      "--lrdecay", "5000", "--bad", model, output_directory, hdf5_file]
+
+        util.run_cmd(self, train_cmd).return_code(0)
+
+        self.assertTrue(os.path.exists(output_directory))
+        self.assertTrue(os.path.exists(os.path.join(output_directory, "model_checkpoint_00000.pkl")))
+        self.assertTrue(os.path.exists(os.path.join(output_directory, "model_checkpoint_00001.pkl")))
+        self.assertTrue(os.path.exists(os.path.join(output_directory, "model_final.pkl")))
+        self.assertTrue(os.path.exists(os.path.join(output_directory, "model.log")))
+        self.assertTrue(os.path.exists(os.path.join(output_directory, "model.py")))
+
+    @parameterized.expand([
+        [2, "small_ch3000_lt0.825_simple2.hdf5", "0"],
+        [5, "small_ch3000_lt0.582_simple5.hdf5", "1"],
+    ])
+    def test_baseline_raw_gru_training(self, stride, hdf5_file_name, subdir):
+        test_work_dir = self.work_dir(os.path.join("test_baseline_raw_gru_training", subdir))
+
+        model = os.path.join(self.models_dir, "baseline_raw_gru.py")
+        self.assertTrue(os.path.exists(model))
+
+        output_directory = os.path.join(test_work_dir, "training_output")
+        if os.path.exists(output_directory):
+            shutil.rmtree(output_directory)
+
+        hdf5_file = os.path.join(self.data_dir, "raw", hdf5_file_name)
+        self.assertTrue(os.path.exists(hdf5_file))
+
+        train_cmd = [self.script, "raw", "--batch_size", "50", "--niteration", "1", "--save_every", "1",
+                     "--lrdecay", "1000", "--winlen", "11", "--stride", str(stride),
+                     "--chunk_len_range", "100", "100", model, output_directory, hdf5_file]
 
         util.run_cmd(self, train_cmd).return_code(0)
 
