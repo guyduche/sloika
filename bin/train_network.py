0fd963e25f898da95f22410dbaa3d34b805f986c
Joe Harvey <joe.harvey@nanoporetech.com>
2017-03-30 18:13:12 +0100

    Higher precision weights keep mtrand happy

--- bin/train_network.py
+++ bin/train_network.py
@@ -190,6 +190,7 @@ if __name__ == '__main__':
             all_weights = h5[args.reweight][:]
         else:
             all_weights = np.ones(len(all_chunks))
+    all_weights = all_weights.astype('float64')
     all_weights /= np.sum(all_weights)
 
     # check chunk_len_range args
