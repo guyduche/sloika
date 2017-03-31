ce5355874283b2560cef236edbb4669c7ff91fd4
jharvey <joe.harvey@nanoporetech.com>
2017-03-31 17:30:49 +0100

    Show more output in workflow.sh

--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -19,7 +19,7 @@ MODEL=$SLOIKA_ROOT/data/test_basecall_network/raw_model_1pt2_cpu.pkl
 echo "# 1. Basecall with existing model"
 export OMP_NUM_THREADS=1
 export THEANO_FLAGS=device=cpu,floatX=float32
-$SLOIKA_ROOT/bin/basecall_network.py raw $MODEL $READ_DIR > to_map.fa
+$SLOIKA_ROOT/bin/basecall_network.py raw $MODEL $READ_DIR | tee to_map.fa
 
 
 echo "# 2. Align reads to reference"
@@ -27,13 +27,13 @@ echo "# 2. Align reads to reference"
 # align.py calls BWA to align the basecalls to the reference
 $SLOIKA_ROOT/misc/align.py $REFERENCE to_map.fa
 # This command extracts a reference sequence for each read using coordinates from the SAM file.
-sloika/misc/refs_from_sam.py --output_strand_list to_map.txt --pad 50 $REFERENCE to_map.sam > to_map_refs.fa
+$SLOIKA_ROOT/misc/refs_from_sam.py --output_strand_list to_map.txt --pad 50 $REFERENCE to_map.sam | tee to_map_refs.fa
 
 
 echo "# 3. Remap reads using existing model"
 export OMP_NUM_THREADS=1
 export THEANO_FLAGS=device=cpu,floatX=float32
-sloika/bin/chunkify.py raw_remap --overwrite --quiet --input_strand_list to_map.txt --downsample 5 $READ_DIR batch_remapped.hdf5 $MODEL to_map_refs.fa
+$SLOIKA_ROOT/bin/chunkify.py raw_remap --input_strand_list to_map.txt --downsample 5 $READ_DIR batch_remapped.hdf5 $MODEL to_map_refs.fa
 
 
 echo "# 4. Train a new model"
@@ -43,7 +43,7 @@ echo "# 4. Train a new model"
 # see TODO: link to wiki
 #export THEANO_FLAGS=openmp=True,floatX=float32,warn_float64=warn,optimizer=fast_run,device=gpu0,lib.cnmem=0.4
 TRAIN_DIR=$WORK_DIR/training
-$SLOIKA_ROOT/bin/train_network.py raw --batch 50 --stride 5 --overwrite --niteration 1 sloika/models/baseline_raw_gru.py $TRAIN_DIR batch_remapped.hdf5
+$SLOIKA_ROOT/bin/train_network.py raw --batch 50 --stride 5 --niteration 1 sloika/models/baseline_raw_gru.py $TRAIN_DIR batch_remapped.hdf5
 
 
 # We exit here as the remaining steps are a work in progress
