80ac3774437ee66d80b94e72045771336add4e65
Joe Harvey <joe.harvey@nanoporetech.com>
2017-05-17 10:51:02 +0100

    Add defaults for shallow network params

--- models/shallow_raw_gru.py
+++ models/shallow_raw_gru.py
@@ -1,7 +1,7 @@
 import sloika.module_tools as smt
 
 
-def network(klen, sd, nfeature, winlen, stride, size=128):
+def network(klen, sd, nfeature=1, winlen=11, stride=5, size=128):
 
     fun = smt.tanh
     init = smt.partial(smt._rn, sd=sd)
