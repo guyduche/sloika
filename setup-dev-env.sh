2594216f331966e25a04678d0e0f37ab9b101ff6
Joe Harvey <joe.harvey@nanoporetech.com>
2017-06-02 16:24:11 +0100

    Remove refs to internal pypi

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -6,17 +6,10 @@ VIRTUALENV_DIR="${DEV_VIRTUALENV_DIR}"
 
 source ${VIRTUALENV_DIR}/bin/activate
 
-# need a version of pip that supports --trusted-host option
 pip install pip --upgrade
 
-pip install \
-    -r misc/requirements.txt \
-    --trusted-host pypi.oxfordnanolabs.local \
-    --index-url https://pypi.oxfordnanolabs.local/simple/
+pip install -r misc/requirements.txt
 
-pip install \
-    -r setup-dev-env.txt \
-    --trusted-host pypi.oxfordnanolabs.local \
-    --index-url https://pypi.oxfordnanolabs.local/simple/
+pip install -r setup-dev-env.txt
 
 python setup.py develop
--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -11,17 +11,11 @@ ${VIRTUALENV_CMD} ${VIRTUALENV_DIR}
 
 source ${VIRTUALENV_DIR}/bin/activate
 
-# need a version of pip that supports --trusted-host option
 pip install pip --upgrade
 
 # install prerequisites of setup.py first
-pip install \
-    -r scripts/requirements.txt \
-    --trusted-host pypi.oxfordnanolabs.local \
-    --index-url https://pypi.oxfordnanolabs.local/simple/
+pip install -r scripts/requirements.txt
 
 # install Sloika's dependencies
-pip install \
-    -r requirements.txt \
-    --trusted-host pypi.oxfordnanolabs.local \
-    --index-url https://pypi.oxfordnanolabs.local/simple/
+pip install -r requirements.txt
+
